691329aa4bf75b8daa26ac55abfe13f703f5f88a
Alesia <alesia.litvinenko@gmail.com>
2022-10-13 23:43:25 +0300

    feat: matrix & mine

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let summ = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      console.log(matrix[i - 1]);
+      // console.log(matrix[i - 1][j]);
+      if (matrix[i - 1] === undefined || matrix[i - 1][j] !== 0) {
+        summ += matrix[i][j];
+      }
+    }
+  }
+  // throw new NotImplementedError('Not implemented');
+  // let arrayMatrix = matrix.flat();
+  // arrayMatrix.forEach((elem) => (summ += elem));
+
+  // console.log(arrayMatrix);
+  console.log(summ);
+  return summ;
   // remove line with error and write your code here
 }
 
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  let array = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    let coutArray = [];
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      if (matrix[i - 1]) {
+        if (matrix[i - 1][j - 1]) count++;
+        if (matrix[i - 1][j]) count++;
+        if (matrix[i - 1][j + 1]) count++;
+      }
+      if (matrix[i][j - 1]) count++;
+      if (matrix[i][j + 1]) count++;
+      if (matrix[i + 1]) {
+        if (matrix[i + 1][j - 1]) count++;
+        if (matrix[i + 1][j]) count++;
+        if (matrix[i + 1][j + 1]) count++;
+      }
+      coutArray.push(count);
+    }
+    array.push(coutArray);
+  }
+  return array;
+  // throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
